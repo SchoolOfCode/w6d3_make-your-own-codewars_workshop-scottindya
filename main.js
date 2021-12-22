cd51bfb579fbc9fe95f9b9757aaee9b6a0406c7c
Indya 2 <indya2@Steves-MacBook-Pro.local>
2021-12-22 15:56:33 +0000

    tests

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 /* 
 👉 Write your kata here!
-*/
+//A man wants to make a million pounds, starts with £n
+//Can work at his first job until doubles his current balance - they pay 10% of current balance per day
+//Second job until he triples current balance etc. - second job will pay 20% of his starting balance per day 
+//Output should be how many different jobs he needs if he starts with n 
 
 //👉 Write the function your CodeWarriors will start with below here:
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,12 @@
 //👉 Write your tests below here:
+test("Check the function correctly calculates the number of days to reach the target", function () {
+  const expected = 15816;
+  const n = 100;
+  const actual = makeAMillion(n);
+  expected(actual).toBe(expected);
+});
+test("Check the function correctly calculates the number of days to reach the target", function () {
+  const expected = 280;
+  const n = 10000;
+  const actual = makeAMillion(n);
+  expected(actual).toBe(expected);
