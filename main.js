10a019ae74a975c38c56081ed0ab47e3d8f13b95
D20CM <scottbrown500@hotmail.com>
2021-12-22 23:26:24 +0000

    add more logic to implementation

--- main.js
+++ main.js
@@ -1,39 +1,59 @@
 
 // 👉 Write your kata here!
 //A man wants to make a million pounds, starts with £n
-//Can work at his first job until doubles his current balance - they pay 10% of starting balance per day (i.e n*0.1)
-//Second job until he triples current balance etc. - second job will pay 20% of his starting balance per day (i.e. n*0.2)
+//Can work at his first job until doubles his current balance - they pay 10% of his initial starting balance per day (i.e n*0.1)
+//Second job until he triples current balance etc. - second job will pay 20% of his initial starting balance per day (i.e. n*0.2)
 //
 //he will work only full days, and only until he makes his million, not a day longer.
-//Output should be how many different jobs he needs if he starts with n 
+//Output should be how many days it will take hime to make his million
 
 //👉 Write the function your CodeWarriors will start with below here:
 
 
 //while dollarsInTheBank < 1000000
 
+    let totalDays = 0;  
+    let salaryMultiplier = 0.1; 
+    let dollarsInTheBank;
+    let jobNumber = 1;
+    let targetMultiplier = 2;
 
-
-function makeAMillion(startingBalance,targetMultiplier){
+export function makeAMillion(startingBalance){
     
-let dollarsInTheBank = startingBalance;
-while(dollarsInTheBank<1000000){
+    dollarsInTheBank = startingBalance;
+        
+    while(dollarsInTheBank<1000000){
+        console.log("Job #" + jobNumber);
+        let target = startingBalance*targetMultiplier;
+        console.log("Target:" +target)
+        //how to fix this as the startingBalance value from the 1st loop???
+        let dailypay = 10000*salaryMultiplier;
+        console.log("DailyPay:" + dailypay)
+        let numberOfDays = (target-startingBalance)/dailypay;
+        
+        console.log("Days worked in this job: " + numberOfDays);
+        dollarsInTheBank = target;
+        console.log("You have thsi much money: $" + dollarsInTheBank)
+
+        //iterate the accumulators for the next loop
+        totalDays = totalDays+numberOfDays;
+        console.log("Total days is: " + totalDays);
+        targetMultiplier ++;
+        salaryMultiplier = salaryMultiplier+0.1;
+        jobNumber ++;
+
+        //if statement here to check if the next loop will take us over a million
+        // if it does, we need different logic (day by day with the current dailyPay up to 1000000)
+         
+
+        //if not, we can run the loop again
+        makeAMillion(target,targetMultiplier);
+        };
 
 
-let salaryMultiplier = 0.1;
-
-let target = startingBalance*targetMultiplier;
-let dailypay = startingBalance*salaryMultiplier;
-let numberOfDays = (target-startingBalance)/dailypay;
-console.log(numberOfDays);
-targetMultiplier ++;
-salaryMultiplier = salaryMultiplier+0.1;
-dollarsInTheBank = dollarsInTheBank+target;
-console.log("You have thsi much money: $" + dollarsInTheBank)
-// dollarsInTheBank = dollarsInTheBank + dailypay*numberOfDays;
-makeAMillion(target);
+console.log(totalDays);
+return totalDays;
 };
-};
 
 
-makeAMillion(10000,2);
+makeAMillion(10000);
--- main.test.js
+++ main.test.js
@@ -1,12 +1,17 @@
 //👉 Write your tests below here:
+
+import {makeAMillion} from './main'
+
 test("Check the function correctly calculates the number of days to reach the target", function () {
   const expected = 15816;
   const n = 100;
   const actual = makeAMillion(n);
-  expected(actual).toBe(expected);
+  expect(actual).toBe(expected);
 });
 test("Check the function correctly calculates the number of days to reach the target", function () {
   const expected = 280;
   const n = 10000;
   const actual = makeAMillion(n);
-  expected(actual).toBe(expected);
+  expect(actual).toBe(expected);
+});
+
