348b30f2a5511577541e362f5bc682908a46485d
D20CM <scottbrown500@hotmail.com>
2021-12-23 14:15:27 +0000

    add kata description

--- main.test.js
+++ main.test.js
@@ -1,3 +1,11 @@
+// 👉 Write your kata here!
+//A man wants to make a million pounds, starts with £n
+//Can work at his first job until doubles his current balance - they pay 10% of his initial starting balance per day (i.e n*0.1)
+//Second job until he triples current balance etc. - second job will pay 20% of his initial starting balance per day (i.e. n*0.2)
+//
+//he will work only full days, and only until he makes his million, not a day longer.
+//Output should be how many days it will take hime to make his million
+
 //👉 Write your tests below here:
 
 import { makeAMillion } from "./main";
